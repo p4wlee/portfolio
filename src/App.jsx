fccb4cde21c11bb4f61b7675b8c8f034f3398084
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-06-24 10:54:27 +0200

    feat: mount Hero section inside main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import StarField from "./components/StarField";
 import useActiveSection from "./hooks/useActiveSection";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 function App() {
   const activeSectionId = useActiveSection();
   return (
     <>
       <StarField />
       <Navbar activeSectionId={activeSectionId} />
+      <main className="content">
+        <Hero />
+      </main>
     </>
   );
 }
