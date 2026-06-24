80fa318e32b7ed2790937ba3b19008e0550e58f5
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-06-24 18:30:33 +0200

    feat: add About section and mount it in main content

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import StarField from "./components/StarField";
 import useActiveSection from "./hooks/useActiveSection";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 function App() {
   const activeSectionId = useActiveSection();
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar activeSectionId={activeSectionId} />
       <main className="content">
         <Hero />
+        <About />
       </main>
     </>
   );
