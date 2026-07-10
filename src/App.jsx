c127d5902c2968ac97463f6e8b36149121b42703
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-07-10 18:32:18 +0200

    feat: mount Projects section in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 function App() {
   const activeSectionId = useActiveSection();
   return (
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <About />
         <Skills />
+        <Projects />
       </main>
     </>
   );
