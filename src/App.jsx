d60fc36be885a572df8f96f39ceaf613c6d43ed7
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-07-15 10:47:35 +0200

    feat: mount Contact section in App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 function App() {
   const activeSectionId = useActiveSection();
   return (
@@ -16,6 +17,7 @@ function App() {
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </main>
     </>
   );
